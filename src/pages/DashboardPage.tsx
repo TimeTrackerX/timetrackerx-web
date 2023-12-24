656c3ca9f66748ee85c66bda2be718754f1dd374
Angel S. Moreno <angelxmoreno@gmail.com>
2023-12-24 01:34:50 -0500

    feat(Routing): integrated react routing and pages

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const DashboardPage: FC = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <h3>Servers</h3>
+        </div>
+    );
+};
+
+export default DashboardPage;
