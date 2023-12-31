75a6bfac943ae8a5e1321ba88fec9c9acffb7694
Angel S. Moreno <angelxmoreno@gmail.com>
2023-12-31 16:03:52 -0500

    chore(Auth): integrated AuthStore with TimeTrackerX SDK

--- src/stores/UseAuthStore.ts
+++ src/stores/UseAuthStore.ts
@@ -1,23 +1,33 @@
+import JwtService from '@app/services/jwt/JwtService.ts';
+import { AuthResponse, AuthUser } from '@timetrackerx/js-sdk';
 import { create } from 'zustand';
-
-export interface AuthUser {
-    id: number;
-    avatar?: string;
-    username: string;
-}
+import { persist } from 'zustand/middleware';
+import type { StateCreator } from 'zustand/vanilla';
 
 type AuthState = {
     user: AuthUser | null;
     isLoggedIn: boolean;
-    updateUser: (authUser: AuthUser) => void;
-    removeUser: () => void;
+    updateAuth: (authResponse: AuthResponse) => void;
+    removeAuth: () => void;
 };
 
-const useAuthStore = create<AuthState>(set => ({
+const stateCreator: StateCreator<AuthState> = set => ({
     user: null,
     isLoggedIn: false,
-    updateUser: (authUser: AuthUser) => set({ user: authUser, isLoggedIn: authUser !== null }),
-    removeUser: () => set({ user: null, isLoggedIn: false }),
-}));
+    updateAuth: async ({ user, token }: AuthResponse) => {
+        await JwtService.set(token);
+        set({ user, isLoggedIn: !!user });
+    },
+    removeAuth: async () => {
+        await JwtService.delete();
+        set({ user: null, isLoggedIn: false });
+    },
+});
+
+const useAuthStore = create(
+    persist(stateCreator, {
+        name: 'authStoreState',
+    }),
+);
 
 export default useAuthStore;
